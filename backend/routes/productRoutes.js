3bd2e92788319ca7b30621ed5e6158090662a6f3
Jason Hargrove <rijaha@gmail.com>
2021-06-29 21:04:47 -0500

    Done with the backend folder.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+  getAllProducts,
+  getProductById
+} = require('../controller/productControllers');
+
 // Get all the products form the db.
 // route get /api/products.
+router.get('/', getAllProducts);
+
+// Get all the products form the db.
+// route get /api/products/:id.
+router.get('/:id', getProductById);
+
+module.exports = router;
